f2e2dd69709fe65ace3fc897b5dd4d124af6527a
R3KLESS <dj-teknike@outlook.com>
2017-03-07 22:33:39 +0000

    master commit

---
+++ alpha Dedicated- DEVELOPMENT/PHP/connection.php
@@ -0,0 +1,19 @@
+<?php
+	
+$servername = 'localhost'; 
+$username = 'user';  
+$password = 'Lexilexi1';
+$dbname = 'u775822918_test';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    echo(json_encode(array('status'=>"Connection failed: " . $conn->connect_error)));
+	die;
+} 
+
+
+
+?>
---
+++ alpha Dedicated- DEVELOPMENT/PHP/registration.php
@@ -0,0 +1,50 @@
+<?php
+
+include_once "connection.php"; 
+
+$mydata = json_decode(file_get_contents('php://input'));
+
+$accountname = $mydata ->accountname; 
+$accountpassword = $mydata ->accountpassword; 
+$accountemail = $mydata ->accountemail; 
+
+if ($stmt = $conn->prepare("SELECT * FROM users WHERE email = ? ")) //check if the email has already been used to register
+{
+    $stmt->bind_param("s", $accountemail);  // "s" means the database expects a string
+
+    $stmt->execute();
+
+    if ($stmt->fetch())  echo  json_encode(array('status'=>'This email adress has already been used to register an account'));
+ 
+        else
+        {
+            if ($stmt = $conn->prepare("SELECT * FROM users WHERE username = ? ")) //check if the account name is already taken
+            {
+                $stmt->bind_param("s", $accountname);  // "s" means the database expects a string
+
+                $stmt->execute();
+
+                if ($stmt->fetch())  echo  json_encode(array('status'=>'This account name is unavailable')); 
+  
+                        else 
+                            {  //create this new account
+
+                            $hash = password_hash($accountpassword, PASSWORD_DEFAULT); //using cool new PHP 5.5 encryption
+
+                            $stmt = $conn->prepare("INSERT INTO users VALUES (NULL, ?, ?, ?)");
+   
+                            $stmt->bind_param("sss", $accountname, $hash, $accountemail);
+   
+                            $stmt->execute();
+   
+                            $stmt->close();
+   
+                            echo json_encode(array('status'=>'OK' ));
+
+                            }
+            }
+       }
+}
+
+
+?>
