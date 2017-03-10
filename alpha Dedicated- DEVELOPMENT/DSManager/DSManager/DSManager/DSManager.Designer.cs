19f3f96614451c313c7b723cef81275bb4901976
R3KLESS <dj-teknike@outlook.com>
2017-03-10 00:12:13 +0000

    added the ds manager to project source

---
+++ alpha Dedicated- DEVELOPMENT/DSManager/DSManager/DSManager/DSManager.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace DSManager
+{
+    partial class DSManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.T_Return = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // T_Return
+            // 
+            this.T_Return.Location = new System.Drawing.Point(58, 53);
+            this.T_Return.Name = "T_Return";
+            this.T_Return.Size = new System.Drawing.Size(161, 20);
+            this.T_Return.TabIndex = 0;
+            // 
+            // DSManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 133);
+            this.Controls.Add(this.T_Return);
+            this.Name = "DSManager";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.DSManager_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox T_Return;
+    }
+}
+
---
+++ alpha Dedicated- DEVELOPMENT/DSManager/DSManager/DSManager/DSManager.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace DSManager
+{
+    public partial class DSManager : Form
+    {
+        public DSManager()
+        {
+            InitializeComponent();
+        }
+
+        private void DSManager_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void CheckCreateRequest()
+        {
+            string post = "";
+            string url = "http://enigmalabs.pe.hu/startLobby.php";
+            string method = "POST";
+            string rc = "";
+
+            rc = ProcessURL(url, method, post);
+            T_Return.Text = rc;
+
+            string[] rcp = rc.Split(new char[1] {';'});
+
+            foreach(string a in rcp)
+            {
+                switch (a)
+                {
+                    case "CS":
+                        Process.Start(@"C:\Users\Pally\Desktop\Online2DS\WindowsNoEditor\Online2\Binaries\Win64\CS");
+                        break;
+                    case "CC":
+                        Process.Start(@"C:\Users\Pally\Desktop\Online2DS\WindowsNoEditor\Online2\Binaries\Win64\CC");
+                        break;
+
+                    case "lobby":
+
+                        // start the lobby executable
+                        Process.Start(@"E:\Unreal Projects\packagedProjects\WindowsNoEditor\alpha\\Binaries\Win64\lobby.lnk");
+
+                        // call the endlobby php script by changing the url
+                        string post2 = "";
+                        string url2 = "http://enigmalabs.pe.hu/endLobby.php";
+                        string method2 = "POST";
+
+                        //call the process url function to end the lobby creation process.
+                        ProcessURL(url2, method2, post2);
+
+                        break;
+
+                    case "PG" :
+                        Process.Start(@"C:\Users\Pally\Desktop\Online2DS\WindowsNoEditor\Online2\Binaries\Win64\PG");
+                        break; 
+
+                }
+            }
+
+        }
+
+        private string ProcessURL(string url, string method, string post)
+        {
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] data = encoding.GetBytes(post);
+            string rc = "";
+            WebRequest request = WebRequest.Create(url);
+            if (post != "")
+            {
+                request.Method = method;
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = data.Length;
+                
+
+                Stream stream = request.GetRequestStream();
+                stream.Write(data, 0, data.Length);
+                stream.Close();
+
+                WebResponse response = request.GetResponse();
+                stream = response.GetResponseStream();
+
+                StreamReader sr = new StreamReader(stream);
+                rc = sr.ReadToEnd();
+            }
+            else
+            {
+                WebResponse response = request.GetResponse();
+                StreamReader sr = new StreamReader(response.GetResponseStream());
+                rc = sr.ReadToEnd();
+            }
+            return rc;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            CheckCreateRequest();
+        }
+    }
+}
