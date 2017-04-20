814c71a13e35c5f8d784104cc676817e666ecc44
Enigma <dj-teknike@outlook.com>
2017-04-20 23:28:14 +0100

    started integrating the mutagen project login system

---
+++ alpha Dedicated- DEVELOPMENT/Source/alpha/mobaGameMode.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "alpha.h"
+#include "mobaGameMode.h"
+
+AmobaGameMode::AmobaGameMode ()
+{
+	InactivePlayerStateLifeSpan = 1;
+	
+}
+
+
+
---
+++ alpha Dedicated- DEVELOPMENT/Source/alpha/mobaGameMode.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "mobaGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALPHA_API AmobaGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+		AmobaGameMode();
+	
+	
+	
+	
+};
